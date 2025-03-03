e4e1b5c24850a90110e0d32cb1593ea27b0d3f37
aadic <aadi.charpe@hotmail.com>
2025-03-02 20:42:59 -0800

    bugfixes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 import requests
 # starting urls
-urls = ['https://www.wikipedia.org/', 'https://www.cnn.com/']
+urls = ['https://www.wikipedia.org/']
 # visited urls
 visited = []
 while urls:
@@ -11,10 +11,15 @@ while urls:
     print('crawling: ' + url)
     # check robots.txt file to see if I can visit url
     canVisit = True
-    response = requests.get('https://' + urlparse(url).netloc + '/robots.txt')
+    relevant = False
+    response = requests.get(urlparse(url).scheme + '://' + urlparse(url).netloc + '/robots.txt')
     disallowed = []
     for line in response.text.splitlines():
-        if line.startswith('Disallow') and len(line.split()) > 1:
+        if line == 'User-agent: *':
+            relevant = True
+        elif line.startswith('User-agent:') and line != 'User-agent: *':
+            relevant = False
+        if line.startswith('Disallow') and len(line.split()) > 1 and relevant:
             disallowed.append(line.split()[1])
     for path in disallowed:
         if urlparse(url).path.startswith(path):
@@ -26,13 +31,13 @@ while urls:
     # find hyperlinks on website
     soup = BeautifulSoup(requests.get(url).content, 'html.parser')
     for link in soup.find_all('a'):
-        if link.get('href').startswith('//'):
-            if 'https:' + link.get('href') not in visited:
-                urls.append('https:' + link.get('href'))
-        elif link.get('href').startswith('http'):
-            if link.get('href') not in visited:
-                urls.append(link.get('href'))
+        if link.get('href') is not None:
+            if link.get('href').startswith('//'):
+                if 'https:' + link.get('href') not in visited:
+                    urls.append('https:' + link.get('href'))
+            elif link.get('href').startswith('http'):
+                if link.get('href') not in visited:
+                    urls.append(link.get('href'))
     visited.append(url)
     # sleep so websites cant tell it's a bot
     time.sleep(1.5)
-print('all urls visited: ' + visited)
