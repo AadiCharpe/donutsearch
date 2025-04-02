274cca48ff687631ea3f633aeeb108f7dca0266f
aadic <aadi.charpe@hotmail.com>
2025-04-01 19:17:34 -0700

    pagerank v1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = tkinter.Tk()
 app.title("donut search")
 app.geometry("600x600")
 app.resizable(False, False)
-
+# create tkinter ui
 example = tkinter.Entry(width=25, font=('',20))
 example.place(x=300, y=30, anchor=tkinter.CENTER)
 
@@ -14,20 +14,34 @@ b.place(x=300, y=100, anchor=tkinter.CENTER)
 
 l = tkinter.Label(font=('',12))
 l.place(x=300, y=140, anchor='n')
+# read index file
+f = open('index.txt', 'r', encoding='utf-8')
+lines = f.readlines()[2:]
+f.close()
+words = {}
+for line in lines:
+    words[line.split('|')[0]] = line.split('|')[1]
 
 def search():
     l.config(text='')
     text = example.get().split()
-    f = open('index.txt', 'r', encoding='utf-8')
-    lines = f.readlines()[2:]
-    f.close()
-    words = {}
-    for line in lines:
-        words[line.split('|')[0]] = line.split('|')[1]
-    matches = ''
+    matches = {}
+    # find all matches
     for t in text:
         if t in words:
-            matches += words[t].replace(',', '\n')
-    l.config(text=matches)
+            for url in words[t].split(','):
+                if url not in matches:
+                    matches[url] = 0
+    # rank the matches based on how many different searched words each page has
+    for key in matches:
+        for t in text:
+            if t in words and key in words[t].split(','):
+                matches[key] += 1
+    newtext = ''
+    # sort the dictionary
+    sorted_items = sorted([(value, key) for key, value in matches.items()])
+    for value, key in reversed(sorted_items):
+        newtext += key
+    l.config(text=newtext)
 
 app.mainloop()
